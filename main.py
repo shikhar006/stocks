4a1fb0b3801355187f1bacfc5c9626113d0c6b33
shikhar006 <agrawalshikhar61@gmail.com>
2020-10-24 12:31:44 +0400

    V 0.3    - Made the website look better    - Added a buy button which will start working in the next update

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ from PIL import ImageTk,Image
 
 # Check for function main
 a = True
+
 # Configure the menu
 root_menu = Tk()
 root_menu.title("MENU")
 root_menu.geometry("200x200")
 root_menu.grid_columnconfigure(2, weight=1)
-
-# Creating the frames (Making it look good)
-frame_lookup = LabelFrame(root_menu, text = "Search for symbol", padx = 1, pady = 1)
-frame_lookup.grid(row = 0, column = 2)
-frame_lookup.grid_columnconfigure(2, weight=1)
 
 # Defining the functions
 def f_lookup():
@@ -50,7 +46,8 @@ def f_lookup():
 				stock_price = stock_price.split(">")
 				stock_price = stock_price[1]
 				stock_price = float(stock_price)
-				stock_label = Label(root_lookup, text = stock_price)
+				stock_price_str = str(stock_price)+ " $"
+				stock_label = Label(root_lookup, text = stock_price_str)
 				stock_label.grid(row = 3, column = 2)
 				time.sleep(3)
 				a = False
@@ -65,7 +62,59 @@ def f_lookup():
 				stock_price = stock_price.split(">")
 				stock_price = stock_price[1]
 				stock_price = float(stock_price)
-				stock_label.config(text = stock_price )
+				stock_price_str = str(stock_price)+ " $"
+				stock_label.config(text = stock_price_str )
+				time.sleep(3)
+				a = True
+		while True:
+			main()
+def f_buy():
+	global a
+	# Configure the lookup menu
+	root_lookup = Tk()
+	root_lookup.title("BUY")
+	root_lookup.geometry("200x200")
+	root_lookup.grid_columnconfigure(2, weight=1)
+
+	# Creating the entry options
+	ent_lookup = Entry(root_lookup, borderwidth = 5)
+	ent_lookup.grid(row = 0, column = 2)
+	
+	# Defining the search button
+	def f_search():
+		global a
+		def main():
+			global a
+			symbol = ent_lookup.get()
+			if a == True:
+				stock_price = ""
+				result = requests.get(f"https://money.cnn.com/quote/quote.html?symb={symbol}").text
+				soup = BeautifulSoup(result, features="lxml")
+				stock_price = soup.find("span",attrs = {"streamformat": "ToHundredth"})
+				stock_price = str(stock_price)
+				stock_price = stock_price.split("<")
+				stock_price = stock_price[1]
+				stock_price = stock_price.split(">")
+				stock_price = stock_price[1]
+				stock_price = float(stock_price)
+				stock_price_str = str(stock_price)+ " $"
+				stock_label = Label(root_lookup, text = stock_price_str)
+				stock_label.grid(row = 3, column = 2)
+				time.sleep(3)
+				a = False
+			else:
+				stock_price = ""
+				result = requests.get(f"https://money.cnn.com/quote/quote.html?symb={symbol}").text
+				soup = BeautifulSoup(result, features="lxml")
+				stock_price = soup.find("span",attrs = {"streamformat": "ToHundredth"})
+				stock_price = str(stock_price)
+				stock_price = stock_price.split("<")
+				stock_price = stock_price[1]
+				stock_price = stock_price.split(">")
+				stock_price = stock_price[1]
+				stock_price = float(stock_price)
+				stock_price_str = str(stock_price)+ " $"
+				stock_label.config(text = stock_price_str )
 				time.sleep(3)
 				a = True
 		while True:
@@ -73,10 +122,17 @@ def f_lookup():
 	# Creating the buttons
 	btn_lookup = Button(root_lookup, text = "Lookup", command = f_search)
 	btn_lookup.grid(row = 1, column = 2, pady = 2, padx = 2)
-
+def f_help():
+	webbrowser.open("https://shikhar006.github.io/stonks/help.html")
 # Creating the buttons
-btn_lookup = Button(frame_lookup, text = "Lookup", command = f_lookup)
+btn_lookup = Button(root_menu, text = "Lookup", command = f_lookup)
 btn_lookup.grid(row = 0, column = 2)
+
+btn_buy = Button(root_menu, text = "Buy Stock", command = f_buy )
+btn_buy.grid(row = 1, column = 2)
+
+btn_help = Button(root_menu, text = "Help", width = 5, command = f_help)
+btn_help.grid(row = 2, column = 2)
 
 # Ending
 root_menu.mainloop()
